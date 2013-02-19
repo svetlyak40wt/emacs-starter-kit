0721b98f231c68b6fa5666e2fe531ddb8ea702b8
Alexander Artemenko <svetlyak.40wt@gmail.com>
2013-02-19 18:21:15 +0400

    Keybindings were reformatted.

--- art.el
+++ art.el
@@ -3,19 +3,13 @@
 (setq inferior-lisp-program "sbcl")
 
 
-
 ;; KEYBINDINGS
 
-(global-set-key "\C-c\C-s" 'slime-selector)
-(global-set-key "\C-c\C-c" 'set-mark-command)
-
-;; (define-key blah-mode-map "\C-c\C-s" 'slime-selector)
-
-;; вместо M-x
-(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key (kbd "C-c C-s") 'slime-selector)
+(global-set-key (kbd "C-c C-c") 'set-mark-command)
 
 ;; используем C-w для удаления слова с опечаткой и последующего набора его заново
 ;; https://sites.google.com/site/steveyegge2/effective-emacs/#item3
-(global-set-key "\C-w" 'backward-kill-word)
-(global-set-key "\C-x\C-k" 'kill-region)
-(global-set-key "\M-z" 'undo)
+(global-set-key (kbd "C-w") 'backward-kill-word)
+(global-set-key (kbd "C-x C-k") 'kill-region)
+(global-set-key (kbd "M-z") 'undo)

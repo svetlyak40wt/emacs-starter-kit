bc2c1fbcae42dc22bb4c7784948f679e74570c18
Alexander Artemenko <svetlyak.40wt@gmail.com>
2013-07-11 15:55:48 +0400

    Настройки для org-mode.

---
+++ art/my-org.el
@@ -0,0 +1,12 @@
+(eval-after-load 'org
+  `(progn
+     (defun org-mode-config ()
+       (local-set-key (kbd "C-c a l") 'org-agenda-list)
+       (local-set-key (kbd "C-c a t") 'org-timeline)
+
+       (setq org-feed-alist
+             '(("Input from evernote"
+                "https://zapier.com/engine/rss/88023/40wt-inbox/"
+                "~/Yandex.Disk.localized/txt/todo.org" "INBOX"))))
+     
+     (add-hook 'org-mode-hook 'org-mode-config)))
